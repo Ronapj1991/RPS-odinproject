81c2079a777d3b3dd9f569b0b47270238fdc7f07
Ron Javellana <Ronapj1991@gmail.com>
2022-12-26 21:49:47 -0500

    add player choice function

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+const CHOICES = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * 3);
     
-    switch(randomIndex) {
-        case 0:
-            return 'rock';
-        case 1:
-            return 'paper';
-        case 2:
-            return 'scissors';
-    }
+    return CHOICES[randomIndex];
 }
 
+function getPlayerChoice() {
+    let playerChoice = prompt("Enter your choice:\nrock\npaper\nscissors");
+    playerChoice = playerChoice.toLowerCase();
+    while(!CHOICES.includes(playerChoice)) {
+        playerChoice = prompt("Please type your choice carefully:\nrock\npaper\nscissors");
+        if (CHOICES.includes(playerChoice)) break;
+    }
+    return playerChoice
+}
