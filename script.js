206258d4096d9679f24d264a37fe650b63afbcc2
Ron Javellana <Ronapj1991@gmail.com>
2022-12-26 22:41:40 -0500

    add main game loop

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const CHOICES = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+
 
 function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * 3);
@@ -19,10 +22,12 @@ function playRound(computer, player) {
     if (computer === CHOICES[0] && player === CHOICES[2] ||
         computer === CHOICES[1] && player === CHOICES[0] ||
         computer ===  CHOICES[2] && player === CHOICES[1]) {
+        computerScore++;
         return `You lose! ${computer} beats ${player}`
     } else if (player === CHOICES[0] && computer === CHOICES[2] ||
                player === CHOICES[1] && computer === CHOICES[0] ||
                player === CHOICES[2] && computer === CHOICES[1]) {
+        playerScore++;
         return "You win!"
     } else {
         return "It's a tie!";
@@ -30,3 +35,22 @@ function playRound(computer, player) {
 }
 
 
+function game() {
+    console.log("Welcome to the Rock Paper Scissors Game!");
+    while(true) {
+        let computer = getComputerChoice();
+        let player = getPlayerChoice();
+        console.log(playRound(computer, player))
+        console.log(`You: ${playerScore}, Computer:${computerScore}`)
+        
+        if (playerScore === 5) {
+            console.log("You are the champion!");
+            break;
+        }
+        
+        if (computerScore === 5) {
+            console.log("Too bad! Try again next time");
+            break;
+        }
+    }
+}
