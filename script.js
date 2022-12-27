f0983c451fa9c67c415614bb3297f308f7d1c006
Ron Javellana <Ronapj1991@gmail.com>
2022-12-26 21:04:01 -0500

    create getComputerChoice function

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let randomIndex = Math.floor(Math.random() * 3);
+    
+    switch(randomIndex) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
+
